862a29ce2b83daacd8d93e5e59ba431cd4452d66
Soichi Hayashi <soichih@gmail.com>
2021-11-23 12:31:38 -0500

    split up into eac RUN

--- r-ergm/build.sh
+++ r-ergm/build.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 
 version=4.2-6775
-docker build --build-arg VERSION=$version -t brainlife/r-ergm .
+docker build --build-arg VERSION=$version -t brainlife/r-ergm:$version .
 docker push brainlife/r-ergm:$version
-docker tag brainlife/r brainlife/r-ergm:latest
+docker tag brainlife/r-ergm brainlife/r-ergm:latest
 docker push brainlife/r-ergm
