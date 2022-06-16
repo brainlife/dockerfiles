98b950256b9432e2ee373c4b02d6328f66868811
Soichi Hayashi <hayashis@iu.edu>
2022-06-16 17:00:05 +0100

    added mne

--- mne/build.sh
+++ mne/build.sh
@@ -2,9 +2,9 @@
 set -e
 
 #tag=0.24dev.1
-tag=1.0.2
+tag=1.0.3
 
 docker build -t brainlife/mne . 
-docker tag brainlife/mne brainlife/mne:$tag
-docker push brainlife/mne
-docker push brainlife/mne:$tag
+#docker tag brainlife/mne brainlife/mne:$tag
+#docker push brainlife/mne
+#docker push brainlife/mne:$tag
