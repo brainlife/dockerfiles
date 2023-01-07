84c4aba0f16ef99a2c4d501d50da1ef815a2f992
Brad Caron <bacaron245@gmail.com>
2023-01-06 23:00:32 -0600

    reason: finish up trekker dockerfiles

--- trekker/v0.4/build.sh
+++ trekker/v0.4/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-tag=0.7
+tag=0.4
 docker build -t brainlife/trekker . 
 docker tag brainlife/trekker brainlife/trekker:$tag
 docker push brainlife/trekker:$tag
--- trekker/v0.6/build.sh
+++ trekker/v0.6/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-tag=0.7
+tag=0.6
 docker build -t brainlife/trekker . 
 docker tag brainlife/trekker brainlife/trekker:$tag
 docker push brainlife/trekker:$tag
