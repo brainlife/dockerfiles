5ed9beb807886b9dfa22d865a83f7f1cade24d2f
Gabriele Amorosino <g.amorosino@gmail.com>
2025-05-19 14:26:43 -0500

    update ants docker to 2.3.1

--- ants/build.sh
+++ ants/build.sh
@@ -1,7 +1,9 @@
-docker pull neurodebian:nd16.04
+docker build -t ants-2.3.1 .
 
-set -e
+# locally tag
+docker tag ants-2.3.1:latest brainlife/ants-docker:2.3.1
 
-docker build -t brainlife/ants . 
-docker tag brainlife/ants brainlife/ants:2.2.0-1bc
-docker push brainlife/ants
+# push to Docker Hub
+docker push brainlife/ants-docker:2.3.1
+# (and optionally:)
+docker push brainlife/ants-docker:latest
--- ants/test.sh
+++ ants/test.sh
@@ -1,1 +1,1 @@
-singularity exec docker://brainlife/ants:2.2.0-1bc antsCorticalThickness.sh
+singularity exec docker://brainlife/ants:2.3.1 antsCorticalThickness.sh
